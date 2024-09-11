03bb31098d30bf9b5b18808ac6d2f51c77656be8
sinan senkul <sinan@sinan-senkul.local>
2024-09-11 11:25:18 +0300

    added configurations

--- AspNetCoreEmpty/Program.cs
+++ AspNetCoreEmpty/Program.cs
@@ -5,7 +5,8 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
-
+app.UseStaticFiles();   //configuration to use static files in wwwroot
+app.UseRouting();        //Routing configuration
 app.MapControllerRoute(
     name: "Default",
     pattern: "{controller=Home}/{action=Index}"
